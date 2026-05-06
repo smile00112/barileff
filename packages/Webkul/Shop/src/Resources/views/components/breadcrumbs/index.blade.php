387050cc792ca825c662a58a6c27e1878c174246
smile00112 <ooo.smile2000@yandex.ru>
2026-05-06 13:57:49 +0300

    fix image crop

--- packages/Webkul/Shop/src/Resources/views/components/breadcrumbs/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/breadcrumbs/index.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <div class="mt-[34px] flex justify-start">
-    <div class="flex items-center gap-x-3.5">        
+    <div class="flex items-center gap-x-3.5">
         {{ Breadcrumbs::view('shop::partials.breadcrumbs', $name, $entity) }}
     </div>
 </div>
